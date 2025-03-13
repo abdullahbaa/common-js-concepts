ace4a0a5405c4d1cf56281a653b773b29d47c87a
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-03-13 12:16:49 +0600

    added primitive codes

---
+++ types.js
@@ -0,0 +1,36 @@
+// Strongly typed language
+// int a = 5;
+// string b= 'alim kalim'
+// Boolean c = true;
+// Object student ={name : 'noya daman',id:44};
+// int[] number = [21,12,33,44,55];
+// string [] friends = ['abui','kabi'];
+
+// js is a Dynamic type language
+
+//primitive type value
+const a =5; 
+const b = 'Shamsu killa kita';
+const d= true;
+
+//Non-primitive type value
+const ages =[34,55,66,77];
+const student ={id:44,name: 'Abdullah'};
+console.log([typeof a,typeof b,typeof d,typeof ages,typeof student]);
+
+let x = 5;
+let y = x;
+console.log(x,y);
+ y= 7;
+console.log(x,y);
+
+ let p ={job:'Web Developer'};
+ let q = p;
+ console.log(p,q);
+ q={job: 'Artchitech'};
+ console.log(p,q);
+
+
+
+
+
