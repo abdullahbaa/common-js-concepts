e66ff1d19f12472f98204683e336e1a6ad618e84
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-03-17 12:54:34 +0600

    added hoeisting scope and closure codes

---
+++ hoeisting.js
@@ -0,0 +1,11 @@
+print5();
+// print10();
+for(let i=0; i<5;i++){
+    console.log(i);
+}
+function print5(){
+    console.log('inside print5',5);
+}
+const print10 = function(){
+    console.log('inside print10',10);
+}
---
+++ scope.js
@@ -0,0 +1,17 @@
+function add (a,b){
+    const total = a+b;
+    console.log(total);
+    if(b>5){
+        const sum =24+a+b;
+    }
+    else{
+        const sum=25+a+b;
+        var current  =sum;
+        console.log(current)
+    }
+    return total;
+    
+}
+// console.log(a,b);
+const fcb=add(5,6);
+console.log(fcb);
