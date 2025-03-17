2e4115a042df35bfdfe5727c27c604b90613c562
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-03-17 11:56:00 +0600

    added double and triple equal codes

---
+++ triple.js
@@ -0,0 +1,21 @@
+// Double equal check kore values ta same ase kina
+// Tripple equal check kore values and types same ase kina.
+// Equal comparison dosent work for non-premitive 
+
+// const first =2; 
+// const second  ='2';
+// const first = 1;
+// const second =true;
+// const first = 0;
+// const second =false;
+const first = [];
+const second =first;
+if(first === second){
+    console.log('values are equal');
+}
+else{
+    console.log('values are not equal');
+}
+// type coersion
+// type conversion
+// type casting
